a790b60ed5eba756a3c9cecc503d24cf4bb46425
Arthur Sousa <sousaarthur840@gmail.com>
2025-09-25 11:58:29 -0300

    Organing files

--- back-end/src/config/db.js
+++ back-end/src/config/db.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";              // importa o mongoose (ODM p/ Mong
 
 export async function connectDB(uri) {        // exporta função assíncrona p/ conectar no banco
   mongoose.set("strictQuery", true);          // exige que buscas usem campos definidos no schema
-  await mongoose.connect(uri);                // abre conexão com o MongoDB usando a URI recebida
+  await mongoose.connect('mongodb+srv://sousaarthur840:sXFWGkNwWCcpknkq@estoque.djdf0fe.mongodb.net/?retryWrites=true&w=majority&appName=Estoque');                // abre conexão com o MongoDB usando a URI recebida
   console.log("MongoDB conectado");           // mostra no terminal que deu tudo certo
 }
